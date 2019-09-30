2f2f4b235adb07825c70d212f23f3bd97575a00f
Brendan Morgenstern <brendan.morgenstern19@stjohns.edu>
2019-09-30 11:52:17 -0400

    Improved testing program to allow for user defined arrays

--- MergeSort/mtest.py
+++ MergeSort/mtest.py
@@ -1,10 +1,26 @@
 from mergesort import *
 import sys
 
+def getIntArray():
+	retlist = []
+	while True:
+		num = input("Enter an integer value to add to the array, or hit Enter to stop inputting values ")
+		if num == "":
+			return retlist
+		try:
+			retlist.append(int(num,10))
+		except ValueError:
+			print(num+" is not a number")
+
 def main(args):
 	Merged = []
+	
 	#hardcoded array given as an example
-	list1 = [123, 34, 189, 56,  150, 12, 9, 240]
+	#list1 = [123, 34, 189, 56,  150, 12, 9, 240]
+	
+	list1 = getIntArray()
+	print("Before sorting: ")
+	print(list1)
 	if len(args) < 2:
 		Merged = MergeSort(list1)
 	else:
@@ -22,6 +38,7 @@ def main(args):
 		print debug info if there any terminal arguments beyond the number of splits
 		'''
 		Merged = MergeSort(list1, splits, debug)
+	print("After sorting : ")
 	print(Merged)
 
 
